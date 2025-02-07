05a4246d47b80982cda04c62622e251d6aed7c70
Xriva18 <kevinguanoluisa2004@gmail.com>
2025-02-07 09:22:30 -0500

    Agregar registro de logs y envío de correo al cambiar el estado de inscripción

--- app/Filament/Resources/InscriptionVerificationResource/Pages/EditInscriptionVerification.php
+++ app/Filament/Resources/InscriptionVerificationResource/Pages/EditInscriptionVerification.php
@@ -7,6 +7,7 @@ use App\Mail\PaymentStatusChanged;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Log;
 
 class EditInscriptionVerification extends EditRecord
 {
@@ -28,9 +29,39 @@ class EditInscriptionVerification extends EditRecord
     {
         $record = $this->record;
 
+        Log::info('Ejecutando afterSave', [
+            'inscription_id' => $record->id,
+            'status' => $record->status
+        ]);
+
         // Verifica si el estado ha cambiado antes de enviar el correo
         if ($record->isDirty('status')) {
-            Mail::to($record->user->email)->send(new PaymentStatusChanged($record));
+            Log::info('El estado ha cambiado', [
+                'previous_status' => $record->getOriginal('status'),
+                'new_status' => $record->status
+            ]);
+
+            try {
+                Mail::to($record->user->email)->send(new PaymentStatusChanged($record));
+
+                Log::info('Correo de estado de pago enviado con éxito.', [
+                    'email' => $record->user->email,
+                    'status' => $record->status,
+                    'inscription_id' => $record->id
+                ]);
+            } catch (\Exception $e) {
+                Log::error('Error al enviar el correo de estado de pago.', [
+                    'email' => $record->user->email,
+                    'status' => $record->status,
+                    'inscription_id' => $record->id,
+                    'error' => $e->getMessage()
+                ]);
+            }
+        } else {
+            Log::info('El estado no ha cambiado, no se enviará correo.', [
+                'inscription_id' => $record->id,
+                'status' => $record->status
+            ]);
         }
     }
 }
